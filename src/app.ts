46fed08afdf5f52acad6eef3c797bf2570902ee3
Dominik <dominikstefancik@seznam.cz>
2019-12-24 15:07:57 +0100

    Set options for debugging

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
 console.log("First lines of Typescript");
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', () => { // compiles with error
+  console.log("Button was clicked");
+})
