66043ad61e65df2d392abcac632a90c00fa60d7e
Dominik <dominikstefancik@seznam.cz>
2019-12-31 15:30:59 +0100

    Get address input from a user

--- projects/select-and-share-place/src/app.ts
+++ projects/select-and-share-place/src/app.ts
@@ -1,1 +1,13 @@
-console.log("Webpack TEST, Mofo");
+const form = document.querySelector("form")!;
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault();
+
+  const addressInput = document.getElementById("address") as HTMLInputElement;
+  const enteredAddress = addressInput.value;
+
+  // send the input address to Google's API
+  
+}
+
+form.addEventListener("submit", searchAddressHandler);
