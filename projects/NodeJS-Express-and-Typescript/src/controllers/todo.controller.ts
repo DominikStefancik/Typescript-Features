c576b8c920c62cf0d6f44e82500f0db06ba27aeb
Dominik <dominikstefancik@seznam.cz>
2020-01-01 18:01:36 +0100

    Add additional CRUD operations

--- projects/NodeJS-Express-and-Typescript/src/controllers/todo.controller.ts
+++ projects/NodeJS-Express-and-Typescript/src/controllers/todo.controller.ts
@@ -1,6 +1,8 @@
-import { RequestHandler } from "express";
+import { Response, RequestHandler } from "express";
 import { Todo } from "../models/todo.model";
 
+type RequestParamsType = { id: string };
+type RequestBodyType = { text: string };
 const TODO_LIST: Todo[] = [];
 
 // "RequestHandler" defines a function type:
@@ -19,3 +21,82 @@ export const createTodo: RequestHandler = (request, response, next) => {
       createdTodo: newTodo
     });
 };
+
+export const getAllTodos: RequestHandler = (request, response, next) => {
+  console.log("Getting all Todo items.");
+
+  response
+    .status(200)
+    .json({
+      todos: TODO_LIST
+    });
+};
+
+export const getTodo: RequestHandler<RequestParamsType> = (request, response, next) => {
+  const todoId = request.params.id;
+  console.log(`Getting a Todo item with id: ${todoId}`);
+
+  const todo = TODO_LIST.find(todo => todo.id === todoId);
+
+  if (!todo) {
+    sendNonExistingTodoResponse(todoId, response);
+    return;
+  }
+  
+  response
+      .status(200)
+      .json({
+        todo
+      });
+};
+
+export const updateTodo: RequestHandler<RequestParamsType> = (request, response, next) => {
+  const todoId = request.params.id;
+  console.log(`Updating a Todo item with id: ${todoId}`);
+
+  const todoIndex = TODO_LIST.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex === -1) {
+    sendNonExistingTodoResponse(todoId, response);
+    return;
+  }
+
+  const updatedText = (request.body as RequestBodyType).text;
+  TODO_LIST[todoIndex] = new Todo(TODO_LIST[todoIndex].id, updatedText);
+  
+  response
+      .status(200)
+      .json({
+        message: "Updated the Todo item.",
+        todo: TODO_LIST[todoIndex]
+      });
+};
+
+export const deleteTodo: RequestHandler<RequestParamsType> = (request, response, next) => {
+  const todoId = request.params.id;
+  console.log(`Deleting a Todo item with id: ${todoId}`);
+
+  const todoIndex = TODO_LIST.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex === -1) {
+    sendNonExistingTodoResponse(todoId, response);
+    return;
+  }
+
+  TODO_LIST.splice(todoIndex, 1);
+  
+  response
+    .status(200)
+    .json({
+      message: "Deleted the Todo item.",
+      todos: TODO_LIST
+    });
+};
+
+const sendNonExistingTodoResponse = (todoId: string, response: Response) => {
+  response
+      .status(400)
+      .json({
+        message: `A Todo with id ${todoId} doesn't exist.`
+      });
+}
--- projects/NodeJS-Express-and-Typescript/src/routes/todo.router.ts
+++ projects/NodeJS-Express-and-Typescript/src/routes/todo.router.ts
@@ -1,22 +1,22 @@
 import { Router } from "express";
-import { createTodo } from "../controllers/todo.controller";
+import { createTodo, getAllTodos, getTodo, updateTodo, deleteTodo } from "../controllers/todo.controller";
 
 // Register middleware (i.e. routes endpoints for incoming requests)
 const router = Router();
 
 // Get all Todos
-router.get("/");
+router.get("/", getAllTodos);
 
 // Get a Todo with the given id
-router.get("/:id");
+router.get("/:id", getTodo);
 
 // Create a Todo
 router.post("/", createTodo);
 
 // Update a Todo with the given id
-router.patch("/:id");
+router.patch("/:id", updateTodo);
 
 // Delete a Todo with the given id
-router.delete("/:id");
+router.delete("/:id", deleteTodo);
 
 export default router;
