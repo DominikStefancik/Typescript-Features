29332785ce3ef13739cff4688cba11998af3a25b
Dominik <dominikstefancik@seznam.cz>
2020-01-04 20:26:49 +0100

    Add React hook to manage state

--- projects/react-and-typescript/src/App.tsx
+++ projects/react-and-typescript/src/App.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./models/todo.model";
 
 const App: React.FunctionComponent = () => {
-  const todoList = [
-    { id: "t1", text: "First TODO item"}
-  ];
+  // "useState" is a React hook which is used for a state management
+  // it returns an array with exactly two elements: 
+  //      first is the latest state snapshot, the second is a function to udate that state and 
+  //      rerender a component
+  const [state, setState] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    
+    setState([{id: Math.random().toString(), text: text}]);
   };
 
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler}/>
-      <TodoList items={todoList}/>
+      <TodoList items={state}/>
     </div>
   );
 }
